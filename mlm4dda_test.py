713b8054144ee520394cc1af3342b4735e277a40
preetkanwalatwal <2363846@brunel.ac.uk>
2026-03-14 12:06:54 +0000

    inital add

---
+++ mlm4dda_test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("C:\\Users\\Owner\\Desktop\\Comp Sci Year 3\\CS3072 Final Year Project\\archive\\cod.csv")
+
+print(df.shape)
+print(df.head())
+print(df.info())
+print(df.describe())
+
